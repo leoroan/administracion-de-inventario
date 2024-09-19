f4a48732836dba0c60ace23bd12baef489a5169d
Leandro <leoroan@gmail.com>
2024-09-19 11:55:55 -0300

    optimizado

--- src/controllers/empleado.controller.js
+++ src/controllers/empleado.controller.js
@@ -8,29 +8,7 @@ export default class EmpleadoController extends GenericController {
   constructor(service) {
     super(service);
   }
-
-  async createAdministrator(req, res) {
-    try {
-      const adminUser = await empleadoService.findByEmailORusername("administrador");
-      if (!adminUser) {
-        await empleadoService.create({
-          username: process.env.ADMIN_USER,
-          password: createHash(process.env.ADMIN_PASS),
-          rol: 'ADMIN',
-          email: process.env.ADMIN_EMAIL,
-          nombre: 'Administrador',
-          apellido: 'General',
-          dni: '00000000'
-        });
-        devLogger.info('[Administrator]: Has been created.');
-      } else {
-        devLogger.info('[Administrator]: Already exists.');
-      }
-    } catch (error) {
-      devLogger.debug(`Error trying to create an Administrator: ${error.message}, Stack: ${error.stack}`);
-    }
-  }
-
+  
   async findByEmailORusername(req, res) {
     try {
       const user = await empleadoService.findByEmailORusername(req.params.some);
