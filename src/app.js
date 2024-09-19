6df36a462e2d32e47a2e653ca50ee8fd0d864d05
Leandro <leoroan@gmail.com>
2024-09-19 11:54:21 -0300

    agregado aftersync

--- src/app.js
+++ src/app.js
@@ -4,26 +4,24 @@ import configureExpress from './config/server/express.config.js';
 import { sequelize } from './services/db/models/setup.db.js';
 import { devLogger } from './config/logger/logger.config.js';
 import { checkConnection } from './config/mail/nodemailer.config.js';
-import EmpleadoController from './controllers/empleado.controller.js'
-import { empleadoService } from './services/service.js';
+import { afterSync } from './config/db/afterSync.db.js';
 
-const empleadoController = new EmpleadoController(empleadoService);
 const app = express();
 configureExpress(app);
 
 const connectWithRetry = () => {
   return sequelize.authenticate()
     .then(() => {
-      devLogger.info(`[DB-Connection]: Success, on port: [${config.db.db_port}]`);
+      devLogger.info(`[DB-CONNECTION]: Success, on port: [${config.db.db_port}]`);
       // return sequelize.sync({ force: true, alter: true });
       return sequelize.sync();
     })
     .then(() => {
-      devLogger.info(`[DB-Models]: Sincronized.`);
+      devLogger.info(`[DB-MODELS]: Sincronized.`);
+      afterSync();
       // console.log(sequelize.models)
       app.listen(process.env.PORT, () => {
-        devLogger.info(`[Server]: Listening on port [${process.env.PORT}]`);
-        empleadoController.createAdministrator();
+        devLogger.info(`[SERVER]: Listening on port [${process.env.PORT}]`);
         checkConnection;
       });
     })
