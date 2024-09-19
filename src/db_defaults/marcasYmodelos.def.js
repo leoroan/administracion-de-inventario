c6fdfb4a01ff2f14be3a65952966bf62af083dec
Leandro <leoroan@gmail.com>
2024-09-19 11:57:00 -0300

    nuevos  valores por defecto para db

---
+++ src/db_defaults/marcasYmodelos.def.js
@@ -0,0 +1,10 @@
+export const marcasPredeterminadas = [
+  {
+    nombre: 'HP',
+    modelos: ['HP-MOD1', 'HP-MOD2', 'HP-MOD3']
+  },
+  {
+    nombre: 'SAMSUNG',
+    modelos: ['SNG-MOD1', 'SNG-MOD2', 'SNG-MOD3']
+  }
+];
---
+++ src/db_defaults/roles.def.js
@@ -0,0 +1,11 @@
+
+export const rolesPredeterminados = [
+  { nombre: 'ADMIN', nivel: 1 },
+  { nombre: 'MINISTRO', nivel: 2 },
+  { nombre: 'SECRETARIO', nivel: 3 },
+  { nombre: 'SUBSECRETARIO', nivel: 4 },
+  { nombre: 'DIRECTOR', nivel: 5 },
+  { nombre: 'DIRECTOR DE LINEA', nivel: 6 },
+  { nombre: 'EMPLEADO', nivel: 7 },
+  { nombre: 'NO_DEFINIDO', nivel: 8 }
+];
