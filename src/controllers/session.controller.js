1a9647d66a7af759c920430d3c81a3db20bc4dfb
Leandro <leoroan@gmail.com>
2024-09-20 21:17:00 -0300

    nueva logica de sesion

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -1,6 +1,7 @@
 import { devLogger } from '../config/logger/logger.config.js';
 import { sessionService } from '../services/service.js';
 import GenericController from './helper/generic.controller.js';
+import { Session } from '../services/db/models/session.model.js';
 
 // Crear un nuevo usuario
 export default class SessionController extends GenericController {
@@ -10,12 +11,18 @@ export default class SessionController extends GenericController {
 
   async initSession(user, token, req, res) {
     try {
-      const newSession = await sessionService.create({
-        tokenSesion: token,
-        fechaInicio: new Date(),
-        usuarioId: user.id
-      });    
-      return newSession;
+      const sesionExistente = await Session.findOne({ where: { empleadoId: user.id } });
+      if (sesionExistente) {
+        await sesionExistente.update({ tokenSesion: token, ultimaConexion: new Date() });
+      } else {
+        const newSession = await sessionService.create({
+          tokenSesion: token,
+          fechaInicio: new Date(),
+          empleadoId: user.id
+        });
+        return newSession;
+      }
+      return sesionExistente;
     } catch (error) {
       devLogger.debug(error)
       return res.sendError(error);
