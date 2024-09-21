129ca68438101c3503ac336dd676a3ceb8816c77
Leandro <leoroan@gmail.com>
2024-09-20 21:17:09 -0300

    probado rutas x nivel de rol

--- src/routes/empleado.router.js
+++ src/routes/empleado.router.js
@@ -24,6 +24,10 @@ export default class EmpleadoExtendRouter extends CustomRouter {
       empleadoController.findAll(req, res);
     });
 
+    this.get('/test/1', ['1'], async (req, res) => {
+      res.send('test role lvl  1');
+    });
+
     this.put('/:id', ['PUBLIC'], async (req, res) => {
       empleadoController.update(req, res);
     });
@@ -36,62 +40,6 @@ export default class EmpleadoExtendRouter extends CustomRouter {
       empleadoController.findByEmailORusername(req, res);
     });
 
-    //     this.get('/:username', ['INSPECTOR', 'ADMIN', 'SUPERVISOR'], async (req, res) => {
-    //       getUserByUsername(req, res);
-    //     });
-
-    //     /**
-    //      * acepta tipos de estado por req.query.estado ('PENDIENTE', 'COMPLETA', 'FIRMADA')
-    //      * el Id del usuario lo recibe del token jwt
-    //      */
-    //     this.get('/usuario/ordenes', ['INSPECTOR', 'ADMIN', 'SUPERVISOR'], passport.authenticate('jwt', { failureRedirect: '/session/fail' }), async (req, res) => {
-    //       getOrdenesDelUsuarioPorEstado(req, res);
-    //     });
-
-    //     /**
-    //      * Acepta tipos de estado por req.query.estado ('PENDIENTE', 'COMPLETA', 'FIRMADA') y tipos de actas por req.query.tipoActa:
-    //      * ActaCalibracion
-    //      * ActaConstatacion
-    //      * ActaDestruccionObleas
-    //      * ActaInspeccion
-    //      * ActaRequerimiento
-    //      * 
-    //      * el Id del usuario lo recibe del token jwt
-    //     */
-    //     this.get('/usuario/actas', ['INSPECTOR', 'ADMIN', 'SUPERVISOR'], passport.authenticate('jwt', { failureRedirect: '/session/fail' }), async (req, res) => {
-    //       getActasDelUsuarioPorEstado(req, res);
-    //     });
-
-    //     /**
-    //      *  Obtiene las actas del usuario ordenadas por fecha de creacion de forma descendente
-    //      *  el Id del usuario lo recibe del token jwt
-    //      */
-    //     this.get('/usuario/actas-full', ['INSPECTOR', 'ADMIN', 'SUPERVISOR'], passport.authenticate('jwt', { failureRedirect: '/session/fail' }), async (req, res) => {
-    //       obtenerTodasLasActasDelUsuarioOrdenadasPorFecha(req, res);
-    //     });
-
-    //     /**
-    //      * getAll() del usuario actual
-    //      * acepta como req.query : page y pageSize para el manjeo de la paginacion
-    //      */
-    //     this.get('/usuario/todo', ['INSPECTOR', 'ADMIN', 'SUPERVISOR'], passport.authenticate('jwt', { failureRedirect: '/session/fail' }), async (req, res) => {
-    //       getAllFromUser(req, res);
-    //     });
-
-    //     /**
-    //      * getAll de todos los users
-    //      * acepta como req.query : page y pageSize para el manjeo de la paginacion
-    //      */
-    //     this.get('/usuarios/todos', ['ADMIN', 'SUPERVISOR'], passport.authenticate('jwt', { failureRedirect: '/session/fail' }), async (req, res) => {
-    //       getAllFromAll(req, res);
-    //     });
-
-    //     /**
-    //      *  ToDo
-    //      */
-    //     // this.get('/usuario/actas-full', ['ADMIN', 'SUPERVISOR'], passport.authenticate('jwt', { failureRedirect: '/session/fail' }), async (req, res) => {
-    //     //   obtenerTodasLasActasOrdenadasPorFecha(req, res);
-    //     // });
 
 
   }
