c0ba097f5297b50850db8feb642319f643b2705c
Leandro Maselli <leoroan@gmail.com>
2024-09-23 14:39:23 -0300

    modificadas las rutas

--- src/routes/empleado.router.js
+++ src/routes/empleado.router.js
@@ -11,39 +11,38 @@ export default class EmpleadoExtendRouter extends CustomRouter {
 
     const empleadoController = new EmpleadoController(empleadoService);
 
-    this.post('/', ['PUBLIC'], async (req, res) => {
+    this.post('/', [3], async (req, res) => {
       empleadoController.create(req, res);
     });
 
-    this.get('/:id', ['PUBLIC'], async (req, res) => {
+    this.get('/:id', [3], async (req, res) => {
       empleadoController.findById(req, res);
     });
 
-    this.get('/', ['PUBLIC'], async (req, res) => {
+    this.get('/', [3], async (req, res) => {
       empleadoController.findAll(req, res);
     });
 
-    this.get('/test/1', [1], async (req, res) => {
-      res.send('test role lvl 1 passed');
-    });
-
-    this.get('/test/4', [4], async (req, res) => {
-      res.send('test role lvl 4 passed');
-    });
-
-    this.put('/:id', ['PUBLIC'], async (req, res) => {
+    this.put('/:id', [4], async (req, res) => {
       empleadoController.update(req, res);
     });
 
-    this.delete('/:id', ['PUBLIC'], async (req, res) => {
+    this.delete('/:id', [3], async (req, res) => {
       empleadoController.delete(req, res);
     });
 
-    this.get('/emailOrUsername/:some', ['PUBLIC'], async (req, res) => {
+    this.get('/emailOrUsername/:some', [3], async (req, res) => {
       empleadoController.findByEmailORusername(req, res);
     });
 
+    // TESTES
+    this.get('/test/1', [1], passport.authenticate('jwt'), async (req, res) => {
+      res.send('test role lvl 1 passed');
+    });
 
+    this.get('/test/4', [4], passport.authenticate('jwt'), async (req, res) => {
+      res.send('test role lvl 4 passed');
+    });
 
   }
 }
