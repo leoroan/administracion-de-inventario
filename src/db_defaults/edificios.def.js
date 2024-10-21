3490aa95da7b88eb6f95a6e3f3822adfcc8abbb5
Leandro Maselli <leoroan@gmail.com>
2024-10-21 13:33:07 -0300

    nueva data

--- src/db_defaults/edificios.def.js
+++ src/db_defaults/edificios.def.js
@@ -1,14 +1,18 @@
 export const edificiosPredeterminados = [
   {
-    nombre: 'HQ',
+    nombre: 'CENTRAL',
     direccion: 'CALLE 10 NRO 1402',
   },
   {
     nombre: 'DIAGONAL',
-    direccion: 'DIAGONAL 77 NRO ????',
+    direccion: 'DIAGONAL 77 NRO 45',
   },
   {
     nombre: 'NUEVO CALLE 6',
-    direccion: 'CALLE 6 NRO ????',
+    direccion: 'CALLE 6 NRO 928',
+  },
+  {
+    nombre: 'CALLE 3 ',
+    direccion: 'CALLE 3 NRO 1630',
   },
 ];
